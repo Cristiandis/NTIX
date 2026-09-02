f45fbe91ece45060b093096ad07afaed6241f195
Cristiandis <112336919+Cristiandis@users.noreply.github.com>
2026-09-02 14:53:54 +0200

    code coverage

--- src/package_manager/mod.rs
+++ src/package_manager/mod.rs
@@ -3,6 +3,6 @@ pub mod command_runner;
 pub mod manager_presence;
 pub mod package_manager_detector;
 pub mod process_command_runner;
-mod table_parser;
+pub mod table_parser;
 pub mod winget_manager;
 pub mod winget_manager_trait;
